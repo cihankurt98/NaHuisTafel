5d668a4398dcf6b8f9be8502319714b0014b2245
Unknown <cihankurt1998@gmail.com>
2019-09-30 13:52:46 +0200

    Added mute button
    Should be able to change sprite after gamemanager's function (on mouse click) is completed.

---
+++ NaHuis/Assets/Scripts/SoundOnOff.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundOnOff : MonoBehaviour
+{
+    public Sprite soundOnSprite;
+    public Sprite soundOffSprite;
+    
+    private void changeSprite()
+    {
+        if (this.GetComponent<SpriteRenderer>().sprite == soundOnSprite)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = soundOffSprite;
+        }
+        else
+        {
+            this.GetComponent<SpriteRenderer>().sprite = soundOnSprite;
+        }
+    }
+    
+
+}
