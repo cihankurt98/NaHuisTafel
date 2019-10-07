446643fb2cc789db578054c69617b6569cfd1e11
Cihan <cihankurt1998@gmail.com>
2019-10-07 12:16:51 +0200

    HangmanController
    Script made, still making progress with text indicators.

--- NaHuis/Assets/Scripts/GameManager.cs
+++ NaHuis/Assets/Scripts/GameManager.cs
@@ -5,10 +5,19 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public Text wordIndicator;
+    public Text lettersIndicator;
+
+    private HangmanController hangman;
+    private string word;
+    private char[] revealed;
+    private bool gameCompleted;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        hangman = GameObject.FindGameObjectWithTag("Player").GetComponent<HangmanController>();
+        ResetGame();
     }
 
     // Update is called once per frame
@@ -17,5 +26,40 @@ public class GameManager : MonoBehaviour
         
     }
 
+    public void Next()
+    {
+        SetWord("test");
+    }
+
+    public void ResetGame()
+    {
+        gameCompleted = false;
+        Next();
+    }
+
+    private void UpdateWordIndicator()
+    {
+        string displayed = "";
+
+        for (int i = 0; i < revealed.Length; i++)
+        {
+            char c = revealed[i];
+            if (c == 0)
+            {
+                c = '_';
+            }
+            displayed += ' ';
+            displayed += c;
+        }
+    }
+
+    private void SetWord(string word)
+    {
+        this.word = word;
+        revealed = new char[word.Length];
+        lettersIndicator.text = "" + word.Length;
+
+        UpdateWordIndicator();
+    }
 
 }
---
+++ NaHuis/Assets/Scripts/HangmanController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HangmanController : MonoBehaviour
+{
+    public GameObject head;
+    public GameObject torso;
+    public GameObject leftarm;
+    public GameObject rightarm;
+    public GameObject rightleg;
+    public GameObject leftleg;
+
+    private int tries;
+    private GameObject[] parts;
+    // Start is called before the first frame update
+    void Start()
+    {
+        parts = new GameObject[] { leftleg, rightleg, rightarm, leftarm, torso, head };
+        tries = parts.Length - 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
