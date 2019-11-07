98f882295311f788b1c5f58a2dda3c7bfefddae1
Cihan <cihankurt1998@gmail.com>
2019-11-07 14:33:04 +0100

    Tic Tac Toe - Player turn function
    Player turn function works. When the game is started it shows the correct player turn.

---
+++ Boter kaas en eieren/Assets/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public int playerTurn; // 0 = player X and 1 = player O
+    public int turnCounter; // counts the number of played turns
+    public GameObject[] turnIcons; // Holding the arrow turn icons for both players
+    public Sprite[] playerIcons; // 0 = player X icon and 1 = player O icon
+    public Button[] tictactoeSpaces; // playable spaces for the game
+    void Start()
+    {
+        GameSetup();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void GameSetup()
+    {
+        playerTurn = 0; // Player X always starts.
+        turnCounter = 0;
+        turnIcons[0].SetActive(true);
+        turnIcons[1].SetActive(false);
+        for (int i = 0; i < tictactoeSpaces.Length; i++)
+        {
+            tictactoeSpaces[i].interactable = true;
+            tictactoeSpaces[i].GetComponent<Image>().sprite = null;
+        }
+    }
+}
