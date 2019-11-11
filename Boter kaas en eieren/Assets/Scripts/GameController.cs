98fa2af0f13d306f408cd4447a3049deec0fec5e
Cihan <cihankurt1998@gmail.com>
2019-11-11 15:35:34 +0100

    Tic Tac Toe - Player icon placement
    Grid is fully functional now. X and O markers can now be placed and the player turn icon changes as well.

--- Boter kaas en eieren/Assets/Scripts/GameController.cs
+++ Boter kaas en eieren/Assets/Scripts/GameController.cs
@@ -32,4 +32,25 @@ public class GameController : MonoBehaviour
             tictactoeSpaces[i].GetComponent<Image>().sprite = null;
         }
     }
+    
+    public void TicTacToeButton(int whichButton)
+    {
+        tictactoeSpaces[whichButton].image.sprite = playerIcons[playerTurn]; // Change empty sprite to X or O
+        tictactoeSpaces[whichButton].interactable = false; // Button can not be used after pressed
+
+       // playerTurn = playerTurn == 0 ? 1 : 0; // Change turn depending on who's turn it actually is.
+
+        if (playerTurn == 0)
+        {
+            playerTurn = 1;
+            turnIcons[0].SetActive(false);
+            turnIcons[1].SetActive(true);
+        }
+        else
+        {
+            playerTurn = 0;
+            turnIcons[0].SetActive(true);
+            turnIcons[1].SetActive(false);
+        }
+    }
 }
