6941882b5a5e9f62aa2db54aa6fd16aa158ce992
Cihan <cihankurt1998@gmail.com>
2020-01-14 16:35:02 +0100

    Math Quiz - Test Build Windows

--- Rekenen/Assets/Scripts/GameManager.cs
+++ Rekenen/Assets/Scripts/GameManager.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
+using B83.ExpressionParser;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,6 +11,7 @@ public class GameManager : MonoBehaviour
     public Text answerBText;
     public Text correctScoreText;
     public Text incorrectScoreText;
+    public Text endCanvasText;
 
     public TextAsset easyFile;
     public TextAsset normalFile;
@@ -24,6 +25,7 @@ public class GameManager : MonoBehaviour
 
     public GameObject menuCanvas;
     public GameObject gameCanvas;
+    public GameObject endCanvas;
 
     // If you want to debug, remove the "//" next to each [SerializeField]
     //[SerializeField]
@@ -40,12 +42,17 @@ public class GameManager : MonoBehaviour
     private int incorrectAnswersCount;
     //[SerializeField]
     private TextAsset textFile;
+    ExpressionParser parser;
 
 
     void Start()
     {
+        Screen.fullScreen = false; // Unity bug: Always starting on fullscreen despite settings
+        parser = new ExpressionParser();
+        correctAnswersCount = 0;
+        incorrectAnswersCount = 0;
+        endCanvas.SetActive(false);
         gameCanvas.SetActive(false); // leave MenuCanvas active
-        linesFromFile = new List<string>();
     }
 
     private void InitGame()
@@ -65,6 +72,7 @@ public class GameManager : MonoBehaviour
 
     private void ConvertFile()
     {
+        linesFromFile = new List<string>();
         string[] tempLinesFromFile = textFile.text.Split('\n'); // Needed because List and Split are not compatible
         foreach (string i in tempLinesFromFile)
         {
@@ -74,10 +82,11 @@ public class GameManager : MonoBehaviour
 
     private void TakeRandomLine()
     {
-        if (linesFromFile.Count == 0 && EditorUtility.DisplayDialog("Uitgespeeld", "Gefeliciteerd! Op dit niveau zijn er geen vragen meer over.", "Terug naar het hoofdmenu"))
+        if (linesFromFile.Count == 0)
         {
             gameCanvas.SetActive(false);
-            menuCanvas.SetActive(true);
+            endCanvas.SetActive(true);
+            endCanvasText.text = "Je hebt " + correctAnswersCount.ToString() + " vragen goed beantwoord en " + incorrectAnswersCount.ToString() + " vragen fout beantwoord.";
         }
 
         // Generate a random integer within the range of the list. Search for that index within the list. 
@@ -89,7 +98,7 @@ public class GameManager : MonoBehaviour
 
     private void GetCorrectAnswer()
     {
-        ExpressionEvaluator.Evaluate<int>(lineFromFile, out answer);
+        answer = (int) parser.Evaluate(lineFromFile);
         if (answer == 0)
         {
             Debug.Log("Calculation Error" + lineFromFile);
@@ -187,5 +196,12 @@ public class GameManager : MonoBehaviour
 
         InitGame();
 
+    }
+
+    public void openMenu()
+    {
+        endCanvas.SetActive(false);
+        gameCanvas.SetActive(false);
+        menuCanvas.SetActive(true);
     }
 }
